efacb56161c0507e1542c710b8ce6c7267253eb1
kurzemnieks-roberts-06 <robertskurzemnieks@gmail.com>
2023-11-24 14:36:47 +0200

    izpildīju pirmo uzdevumu

--- 1uzd.py
+++ 1uzd.py
@@ -2,12 +2,9 @@
 """					
 Izveidojiet Python programmu, kas saskaita no 1 līdz lietotāja ievadītam skaitlim, izmantojot for ciklu.
 """
-class A:
-    def __init__(self):
-        
-        
-        
-    for():
-        while
-        
-        print()
+summa=0
+a=int(input("ievadi skaitli..."))
+c=a+1
+for i in range(1,c,1):
+    summa=sum+1
+    print(summa)

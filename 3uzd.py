ca8abd52ca6b36a9d9752595b9279ba13dda027a
kurzemnieks-roberts-06 <robertskurzemnieks@gmail.com>
2023-11-24 14:29:45 +0200

    izpildīju 3 uzdevumu

--- 3uzd.py
+++ 3uzd.py
@@ -1,3 +1,8 @@
 """
 Izveidojiet Python programmu, kas pārbauda, vai ievadītais skaitlis ir nepāra, izmantojot if priekšrakstu.
 """
+a=int(input("Ievadi sakaitli "))
+if a%2==0:
+    print(f"Jūs, esat ievadījis pāra skaitli!",a)
+if a%2==1:
+    print(f"Jūs, ievadījāt nepāra skaitli!", a)

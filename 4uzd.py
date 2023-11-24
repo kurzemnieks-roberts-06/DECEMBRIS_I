ee4e8b674f2295c0bb88e0e9900eb44ffc495020
kurzemnieks-roberts-06 <robertskurzemnieks@gmail.com>
2023-11-24 14:42:24 +0200

    izpildīju ceturto uzd

--- 4uzd.py
+++ 4uzd.py
@@ -2,3 +2,14 @@
 Izveidojiet Python programmu, kas aprēķina lietotāja ievadīta skaitļa faktoriālu, izmantojot for ciklu.
 """
 
+def faktorials(skaitlis):
+    rezultats = 1
+    for i in range(1, skaitlis + 1):
+        rezultats *= i
+    return rezultats
+
+# Lietotāja ievade
+ievaditais_skaitlis = int(input("Ievadiet skaitli, lai aprēķinātu faktoriālu: "))
+
+# Aprēķina faktoriālu un izvada rezultātu
+print(f"Faktoriāls no {ievaditais_skaitlis} ir: {faktorials(ievaditais_skaitlis)}")
